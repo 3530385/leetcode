56b04a04a23d80a94423954f9a4591d7c5f3b9d5
Евгений Шевченко <3530385@gmail.com>
2023-03-20 13:42:19 +0300

    task 28 complexity plot add

--- easy/28. Find the Index of the First Occurrence in a String/task_28.py
+++ easy/28. Find the Index of the First Occurrence in a String/task_28.py
@@ -1,6 +1,9 @@
+from time_compexity_plot.plot import plot_time_complexity
+
+
 def sub_idx(haystack: str, needle: str) -> int:
     return haystack.find(needle)
 
 
 if __name__ == '__main__':
-    print(sub_idx(haystack="sadbutsad", needle="sad"))
+    plot_time_complexity(lambda n: sub_idx(haystack="abcdef" * (n // 6) + "sad", needle="sad"))
